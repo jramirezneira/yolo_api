1c8ee8df09c92b55766f4861466a56f8edc60447
jramirezneira <jramirezneira@gmail.com>
2024-07-24 18:18:33 +0200

    con rtsp

--- server/utils/stream_loaders.py
+++ server/utils/stream_loaders.py
@@ -43,7 +43,7 @@ class LoadStreamNoThread:
             # import pafy
             # source = pafy.new(source).getbest(preftype='mp4').url   
             cmd="python3 stream_rtsp_server.py"
-            self.thr = threading.Thread(target=self.startStreamRtspServer, args=(cmd), kwargs={})
+            self.thr = threading.Thread(target=self.startStreamRtspServer, args=(), kwargs={})
             self.thr.start()  
             source="rtsp://127.0.0.1:8554/video_stream"
         self.cv2= cv2       
@@ -63,8 +63,8 @@ class LoadStreamNoThread:
 
         return None
     
-    def startStreamRtspServer(self, cmd):        
-        self.proc = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, shell=True)
+    def startStreamRtspServer(self):        
+        self.proc = subprocess.Popen("python3 stream_rtsp_server.py", stdout=subprocess.PIPE, shell=True)
         out, err = self.proc.communicate() 
         result = out.split('\n')
         for lin in result:
