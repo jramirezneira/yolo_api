e475b2d4e841b21e6a4095bde4fd0b614efa088c
jramirezneira <jramirezneira@gmail.com>
2024-07-24 19:02:18 +0200

    con rtsp

--- server/server.py
+++ server/server.py
@@ -99,8 +99,11 @@ def cv2DestroyAllWindows():
 
         if isinstance(obj, Popen):
             try:
+                subprocess.Popen.poll(obj)
+                LOGGER.info("status Popen %s " % subprocess.Popen.poll(obj))
                 subprocess.Popen.terminate(obj)
                 LOGGER.info("Popen %s " % obj)
+                LOGGER.info("status Popen %s " % subprocess.Popen.poll(obj))
             except Exception as e:
                 LOGGER.error("An exception occurred in subprocess.Popen.terminate : %s" % e)
 
